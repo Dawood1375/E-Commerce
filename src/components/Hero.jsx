b61e1066d2c0e6463c5c51e452c1f36a58f77bc7
Dawood1375 <daudashraf1375@gmail.com>
2024-10-29 02:06:11 -0700

    seconf commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,25 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
- 
- <>
- 
-<div id="controls-carousel" className="mt-6 relative w-full" data-carousel="static">
-    {/* <!-- Carousel wrapper --> */}
-    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-        
-        
-        {/* <!-- Item 2 -->  */}
-        <div  data-carousel-item="active">
-            <img src="https://images.pexels.com/photos/2977345/pexels-photo-2977345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="mt-8 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+    <div id="controls-carousel" className="relative w-full mt-6 z-10">
+      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div data-carousel-item="active">
+          <img
+            src="https://images.pexels.com/photos/2977345/pexels-photo-2977345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="block w-full object-cover"
+            alt="Hero"
+          />
         </div>
-        
+      </div>
     </div>
-    
-</div>
-
- </>
-     
   )
 }
 
